3701e74a81945103548a20431c5fa85574218911
Gomez <gomez.molina.alma.rosa@gmail.com>
2023-12-18 19:24:12 -0600

    combates y back

--- poke-apif/app/battle/page.js
+++ poke-apif/app/battle/page.js
@@ -1,14 +1,38 @@
 import Link from "next/link";
-
+import commonStyles from '../user/common-styles.module.css';
 export default function battle ( ) {
   return (
     <>
-  <div className="battle-content">
+
+  <div className={`${commonStyles.backgroundContainercombat} ${commonStyles.pageContainercombat}`}>
     <h1 className="battletitles">Seccion de combates</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna lectus, pellentesque quis cursus accumsan, posuere in erat. Vestibulum vel pellentesque sapien, vitae feugiat ex. Mauris eget suscipit orci, et porttitor sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec accumsan felis, quis egestas ex. Vestibulum rutrum dolor ante, ut sodales metus efficitur eu. Fusce convallis tortor in augue lacinia, sed placerat mauris ullamcorper. Duis rhoncus ut enim sit amet blandit.</p>
       <Link className="btn10" href={"/battle/starbattle"}>
                     Iniciar Batalla
       </Link>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+     
   </div>
   </>
   )
