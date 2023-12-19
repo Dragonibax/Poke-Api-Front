7a7983551e58f136af0d04344fab230fa9dca7e9
Abraham Gonzalez <abrahamgonz93@gmail.com>
2023-12-19 16:57:25 -0600

    se agrego la descripcion de la parte de batalla

--- poke-apif/app/battle/page.js
+++ poke-apif/app/battle/page.js
@@ -1,18 +1,38 @@
 import Link from "next/link";
 
-export default function battle ( ) {
+export default function Battle() {
   return (
     <>
-  <div className="battle-content">
-    <h1 className="battletitles">Seccion de combates</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna lectus, pellentesque quis cursus accumsan, posuere in erat. Vestibulum vel pellentesque sapien, vitae feugiat ex. Mauris eget suscipit orci, et porttitor sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec accumsan felis, quis egestas ex. Vestibulum rutrum dolor ante, ut sodales metus efficitur eu. Fusce convallis tortor in augue lacinia, sed placerat mauris ullamcorper. Duis rhoncus ut enim sit amet blandit.</p>
-      <Link className="btn10" href={"/battle/starbattle"}>
-                    Iniciar Batalla
-      </Link>
-  </div>
-  </>
-  )
+      <div className="battle-content">
+        <h1 className="battletitles">Sección de combates</h1>
+
+        <p>
+          Bienvenidos a nuestra Poke-Api, el funcionamiento será el siguiente: Al registrarte obtendrás un cierto número de tiempo,
+          ese tiempo te servirá para sobrevivir y para apostar. Cada día habrá una reducción de tiempo para simular
+          los gastos diarios en Agua, Comida, etc.
+        </p>
+        <br></br> 
+        <p>Estos son los pasos a seguir dentro de los combates:</p>
+        <br></br>
+
+        <ol style={{ listStylePosition: "inside" }}>
+          <li style={{ textAlign: "center", marginLeft: "1em" }}>Presiona iniciar batalla.</li>
+          <li style={{ textAlign: "center", marginLeft: "1em" }}>Selecciona al Pokémon con el que deseas combatir.</li>
+          <li style={{ textAlign: "center", marginLeft: "1em" }}>Selecciona un ataque para tu Pokémon.</li>
+          <li style={{ textAlign: "center", marginLeft: "1em" }}>Selecciona el tiempo a apostar.</li>
+          <li style={{ textAlign: "center", marginLeft: "1em" }}>Combate.</li>
+        </ol>
+        <br></br>
+        <p>Una vez finalizado el combate, tendrás el resultado de la batalla.</p>
+
+        <Link className="btn10" href={"/battle/starbattle"}>
+          Iniciar Batalla
+        </Link>
+      </div>
+    </>
+  );
 }
+
 
 
 
